d9e7447c7cee3e6e3c0964e22e0942d6f2f407dd
RazanZai <razane.bouhmidi@gmail.com>
2025-01-27 15:24:05 +0100

    toto

---
+++ ProjectFiles/NetSolution/DesignTimeNetLogic1.cs
@@ -0,0 +1,27 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.UI;
+using FTOptix.NativeUI;
+using FTOptix.HMIProject;
+using FTOptix.RAEtherNetIP;
+using FTOptix.Retentivity;
+using FTOptix.CoreBase;
+using FTOptix.CommunicationDriver;
+using FTOptix.Core;
+using FTOptix.NetLogic;
+using FTOptix.DataLogger;
+using FTOptix.Store;
+using FTOptix.SQLiteStore;
+using FTOptix.WebUI;
+#endregion
+
+public class DesignTimeNetLogic1 : BaseNetLogic
+{
+    [ExportMethod]
+    public void Method1()
+    {
+        // Insert code to be executed by the method
+    }
+}
